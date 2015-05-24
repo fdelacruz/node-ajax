d59aa5baf3ba242638607f0867c6f96f859c01d1
Francisco De La Cruz <helloquico@gmail.com>
2015-05-23 22:57:50 -0400

    Send and Receive Data

---
+++ public/javascripts/main.js
@@ -0,0 +1,20 @@
+$(document).on('ready', function() {
+
+	var count = 0;
+
+	$('#ajax-button').on('click', function() {
+
+		// Make an AJAX request
+		var toSend = {
+			message: 'Hello ' + count
+		};
+
+		$.post('/users', toSend, function(data) {
+			console.log(data);
+		});
+
+		// increment the count each time the button is clicked
+		count++;
+	});
+
+});
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function(req, res, next) {
+	var message = req.param('message');
+	res.send('Message: ' + message);
 });
 
 module.exports = router;
